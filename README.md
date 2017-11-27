d0fab8c1c507c9c7dca94e278b53fbd2ee2b1d8b
Shinya Tahara <shinya@financive.com>
2017-11-27 10:18:17 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# slack-butlerbot
+# butlerbot
+A chat bot for [Slack](https://slack.com) based on [slackbot](https://github.com/lins05/slackbot).
+
+## Acknowledgements
+
+Inspiration and code was taken from many sources, including:
+* [@pyconjp](https://github.com/pyconjp) (PyCon JP)
+  [https://github.com/pyconjp/pyconjpbot](https://github.com/pyconjp/pyconjpbot)
---
+++ butlerbot/plugins/greeting.py
@@ -0,0 +1,63 @@
+#coding: UTF-8
+from random import choice
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+from ..botmessage import botreply
+
+
+@listen_to('おはよう|お早う')
+def morning(message):
+    replies = (
+        'おはようございます！ :smily:',
+        '気持ちのいい朝ですね！ :sunny:',
+        'Good morning!',
+    )
+    botreply(message, choice(replies))
+
+
+@listen_to('こんにち[はわ]')
+def noon(message):
+    replies = (
+        'こんにちは！ :smily:',
+        'こんにちは、調子はどうかな :smily:',
+        'Hi!',
+    )
+    botreply(message, choice(replies))
+
+
+@listen_to('いってきま|行ってきま|行ってくる')
+def go(message):
+    replies = (
+        'はい、お気をつけて！ :wave:',
+        'いってらっしゃい！ :wave:',
+        'Have a good day!',
+    )
+    botreply(message, choice(replies))
+
+
+@listen_to('眠た?い|ねむた?い|寝る|寝ます|おやすみ')
+def night(message):
+    replies = (
+        'おやすみなさい :crescent_moon:',
+        'それでは良い夢を :sheep:',
+        'Have a good night!',
+    )
+    botreply(message, choice(replies))
+
+@listen_to('帰る[よね]|帰りま|帰ってる')
+def goback(message):
+    replies = (
+        'お疲れさま、お気をつけて :wave:',
+        'お帰りをお待ちしてます :smily:',
+        'Watch your step going home!',
+    )
+    botreply(message, choice(replies))
+
+@respond_to('ありがと|さんきゅ|サンキュ|どうも')
+def thanks(message):
+    replies = (
+        'どういたしまして！',
+        'お役に立てたのなら何よりです :smily:',
+        'It\'s my pleasure!',
+    )
+    botreply(message, choice(replies))
